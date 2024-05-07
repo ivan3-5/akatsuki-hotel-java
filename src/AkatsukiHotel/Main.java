c51399aa568f4331fb72f393a9d988bd8bdad45c
Ivan Adcan <ivan adcan>
2024-05-07 20:37:16 +0800

    First Commit Hotel Management Reservation System.

---
+++ src/AkatsukiHotel/Main.java
@@ -0,0 +1,12 @@
+package AkatsukiHotel;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Login frameLogin = new Login();
+        frameLogin.setVisible(true);
+        frameLogin.setLocationRelativeTo(null);
+        frameLogin.setResizable(false);
+    }
+}
